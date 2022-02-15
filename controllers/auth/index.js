2c20f18859e4240d919244d305acfcd3e358926e
Oleh-Mohylnyi <oleh.mohylnyi@gmail.com>
2022-02-16 00:54:12 +0200

    standup1

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -76,80 +76,79 @@ import { nanoid } from 'nanoid'
 import jwt from 'jsonwebtoken'
 import User from '../../model/user'
 
-const googleAuth = async (req, res) => {
-  const stringifiedParams = queryString.stringify({
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
-    scope: [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile',
-    ].join(' '),
-    response_type: 'code',
-    access_type: 'offline',
-    prompt: 'consent',
-  })
-  return res.redirect(
-    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`,
-  )
-}
+// const googleAuth = async (req, res) => {
+//   const stringifiedParams = queryString.stringify({
+//     client_id: process.env.GOOGLE_CLIENT_ID,
+//     redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
+//     scope: [
+//       'https://www.googleapis.com/auth/userinfo.email',
+//       'https://www.googleapis.com/auth/userinfo.profile',
+//     ].join(' '),
+//     response_type: 'code',
+//     access_type: 'offline',
+//     prompt: 'consent',
+//   })
+//   return res.redirect(
+//     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`,
+//   )
+// }
 
-const googleRedirect = async (req, res) => {
-  const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
-  const urlObj = new URL(fullUrl)
-  const urlParams = queryString.parse(urlObj.search)
-  const code = urlParams.code
-  const tokenData = await axios({
-    url: `https://oauth2.googleapis.com/token`,
-    method: 'post',
-    data: {
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
-      grant_type: 'authorization_code',
-      code,
-    },
-  })
+// const googleRedirect = async (req, res) => {
+//   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+//   const urlObj = new URL(fullUrl)
+//   const urlParams = queryString.parse(urlObj.search)
+//   const code = urlParams.code
+//   const tokenData = await axios({
+//     url: `https://oauth2.googleapis.com/token`,
+//     method: 'post',
+//     data: {
+//       client_id: process.env.GOOGLE_CLIENT_ID,
+//       client_secret: process.env.GOOGLE_CLIENT_SECRET,
+//       redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
+//       grant_type: 'authorization_code',
+//       code,
+//     },
+//   })
 
-  const userData = await axios({
-    url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-    method: 'get',
-    headers: {
-      Authorization: `Bearer ${tokenData.data.access_token}`,
-    },
-  })
+//   const userData = await axios({
+//     url: 'https://www.googleapis.com/oauth2/v2/userinfo',
+//     method: 'get',
+//     headers: {
+//       Authorization: `Bearer ${tokenData.data.access_token}`,
+//     },
+//   })
 
-  // -------------------Логика-------------------
+//   // -------------------Логика-------------------
 
-  const { email, picture, id, name } = userData.data
-  let user = await User.findOne({ email })
-  const avatar = picture
-
-
-  // if (!user) {
-  //   const verifyToken = nanoid()
-  //   const password = nanoid(32)
-  //   // user = new User({ email, verifyToken })
-  //    // user = new User({ id, email, avatarURL: picture, verifyToken })
-
-  //   user.setPassword(password)
-    await user.save()
-  }
-
-  const payload = {
-    email,
-  }
-
-  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY)
-  user = await User.findByIdAndUpdate(user._id, { token })
-
-  return res.redirect(
-    `${process.env.FRONTEND_URL}?token=${token}&email=${email}`,
-    // `${process.env.FRONTEND_URL}/google-redirect?token=${token}?email=${email}`
+//   const { email, picture, id, name } = userData.data
+//   let user = await User.findOne({ email })
+//   const avatar = picture
 
 
-    // `${process.env.FRONTEND_URL}/google-redirect?email=${email}`
-  )
-}
+//   // if (!user) {
+//   //   const verifyToken = nanoid()
+//   //   const password = nanoid(32)
+//   //   // user = new User({ email, verifyToken })
+//   //    // user = new User({ id, email, avatarURL: picture, verifyToken })
+
+//   //   user.setPassword(password)
+//     await user.save()
+//   }
+
+//   const payload = {
+//     email,
+//   }
+
+//   const token = jwt.sign(payload, process.env.JWT_SECRET_KEY)
+//   user = await User.findByIdAndUpdate(user._id, { token })
+
+//   return res.redirect(
+//     `${process.env.FRONTEND_URL}?token=${token}&email=${email}`,
+//     // `${process.env.FRONTEND_URL}/google-redirect?token=${token}?email=${email}`
+
+
+//     // `${process.env.FRONTEND_URL}/google-redirect?email=${email}`
+//   )
 
 const verifyToken = (token) => {
   try {
